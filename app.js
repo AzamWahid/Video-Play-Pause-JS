5b78d1fdf4eccd3b1d36858a22f52c1fa280596c
azam.wahid <azamwahid@outlook.com>
2025-01-03 02:08:50 +0500

    inital commit with funcationality added

---
+++ app.js
@@ -0,0 +1,48 @@
+// Array of video objects
+const videos = [
+    { title: "Video 1", url: "./assets/1st.mp4", thumbnail: './assets/title/1stTitle.png' },
+    { title: "Video 2", url: "./assets/2nd.mp4", thumbnail: './assets/title/2ndTitle.png'},
+    { title: "Video 3", url: "./assets/3rd.mp4", thumbnail: './assets/title/3rdTitle.png'},
+];
+
+// Function to populate the video list
+function populateVideoList() {
+    const videoList = document.querySelector('.list-unstyled');
+    videos.forEach((video, index) => {
+        
+
+        videoList.innerHTML += `<div class="card" style="width: 18rem;">
+  <img src="${video.thumbnail}" class="card-img-top" alt="..." onclick="changeVideo('${video.url}')">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
+    });
+}
+
+// Function to change the video source
+function changeVideo(url) {
+    const videoElement = document.getElementById("myVideo");
+    const videoSource = document.getElementById("videoSource");
+    console.log(url)
+    videoSource.src = url;
+    videoElement.load(); // Reload the video to change the source
+    videoElement.play(); // Auto-play the video after source change
+}
+
+// Play/Pause button functionality
+function playPause() {
+    const video = document.getElementById("myVideo");
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+// Populate the video list when the page loads
+window.onload = function () {
+    populateVideoList();
+};
